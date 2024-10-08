c3bc03ecf7a98e74cf4ada034a313ee6bb24ec46
363045841 <161981174@qq.com>
2024-10-08 12:51:32 +0800

    搜索加载条宽度响应式调整

--- src/components/search/loading.vue
+++ src/components/search/loading.vue
@@ -7,19 +7,28 @@
     height="250px"
     style="display: flex; justify-content: center; align-items: center; border-radius: 16px;"
   >
-    <v-progress-linear
+    <v-progress-linear 
       :model-value="loadrate"
       rounded
       indeterminate
-      style="width: 10%"
+      :style="loadWidth"
     ></v-progress-linear>
   </v-list>
 </template>
 
 <script setup lang="ts" name="">
 import { ref } from "vue";
-let loading = ref<boolean>(true);
+import { useViewPortStore } from "@/stores/viewportState";
+
+
+const viewportState = useViewPortStore();
+let loadWidth = (viewportState.isMobile ? {width : "50%"} : {width : "10%"})
+watch(() => viewportState.isMobile, () => {
+  loadWidth = (viewportState.isMobile ? {width : "50%"} : {width : "10%"})
+})
+
 let loadrate = ref<number>(20);
+
 </script>
 
 <style scoped></style>
