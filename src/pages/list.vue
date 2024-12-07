ae2f02fe0159e99fd710f6055efb51e66988bbb3
363045841 <161981174@qq.com>
2024-12-07 21:57:18 +0800

    /list 切换页面及基本响应式界面

--- src/pages/list.vue
+++ src/pages/list.vue
@@ -1,8 +1,9 @@
 <template>
   <v-container class="pa-4">
     <v-row>
+      <!-- 大屏幕一行6个,中屏幕一行4个,小屏幕一行2个,行数由视口高度决定 -->
       <v-col
-        v-for="(card, index) in list"
+        v-for="(card, index) in showCardArray"
         :key="index"
         cols="6"
         sm="6"
@@ -48,48 +49,41 @@
     :length="10"
     class="fixed-pagination"
     :total-visible="7"
-    rounded
+    v-model:model-value="page"
+    ref="pagination"
   ></v-pagination>
   <v-pagination
     v-if="ViewPort.isTablet"
     :length="7"
     class="fixed-pagination"
     :total-visible="7"
-    rounded
+    v-model:model-value="page"
+    ref="pagination"
   ></v-pagination>
   <v-pagination
     v-if="ViewPort.isMobile"
     :length="5"
     class="fixed-pagination"
     :total-visible="7"
-    rounded
+    v-model:model-value="page"
+    ref="pagination"
   ></v-pagination>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue";
 import { useViewPortStore } from "@/stores/viewportState";
-import { parseCache } from "vue/compiler-sfc";
 
-// 拿DOM测试，如果图片大小不一致再考虑
-/* let picSize = {
-  picWidth: 0,
-  picHeight: 0,
-}
-const testDOM = useTemplateRef("testDOM");
-const listImage = useTemplateRef("listImage");
-onMounted(() => {
-  console.log(testDOM.value);
-  console.log(listImage.value[0]);
-}); */
-
-const ViewPort = useViewPortStore();
-
-interface tcm {
-  name: string;
-  detail: string;
-  picurl?: string;
-}
+let viewPortStore = useViewPortStore();
+let page = ref(1);
+let screenInfo = reactive({
+  width: 0,
+  height: 0,
+  showBegin: 0,
+  showEnd: 0,
+});
+import { VContainer } from "vuetify/components";
+const pagination = ref<InstanceType<typeof VContainer> | null>(null);
 
 let list = reactive<tcm[]>([
   {
@@ -103,12 +97,72 @@ let list = reactive<tcm[]>([
     picurl:
       "http://www.zhongyoo.com//uploads/allimg/140302/1-1403021110535M.jpg",
   },
-  { name: "鹿鞭", detail: "鹿鞭是补气养生的佳品" },
-  { name: "鹿胎", detail: "鹿胎是补气养生的佳品" },
-  { name: "鹿胎", detail: "鹿胎是补气养生的佳品" },
-  { name: "鹿胎", detail: "鹿胎是补气养生的佳品" },
-  { name: "鹿胎", detail: "鹿胎是补气养生的佳品" },
+  { name: "鹿鞭1", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿胎2", detail: "鹿胎是补气养生的佳品" },
+  { name: "鹿胎3", detail: "鹿胎是补气养生的佳品" },
+  { name: "鹿胎4", detail: "鹿胎是补气养生的佳品" },
+  { name: "鹿胎5", detail: "鹿胎是补气养生的佳品" },
+  { name: "鹿鞭6", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭7", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭8", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭9", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭10", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭11", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭12", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭13", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭14", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭15", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭16", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭17", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭18", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭19", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭20", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭21", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭22", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭23", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭24", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭25", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭26", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭27", detail: "鹿鞭是补气养生的佳品" },
+  { name: "鹿鞭28", detail: "鹿鞭是补气养生的佳品" },
 ]);
+watch(page, (val) => {
+  screenInfo.width = (viewPortStore.isDesktop) ? 6 : viewPortStore.isTablet ? 4 : 2;
+  const viewportHeight = window.innerHeight - 20 - 64 - 10;
+  //const containerHeight = container.value?.$el.clientHeight;
+  screenInfo.height = Math.floor(viewportHeight / (16 * 0.01 * viewportHeight + 100));
+  screenInfo.showBegin = (page.value - 1) * (screenInfo.width * screenInfo.height)
+  screenInfo.showEnd = screenInfo.showBegin + screenInfo.width * screenInfo.height;
+  if(screenInfo.showEnd > list.length) screenInfo.showEnd = list.length;
+  console.log(screenInfo);
+}, { immediate: true });
+
+let showCardArray = computed(() => {
+  console.log("update");
+  return list.slice(screenInfo.showBegin, screenInfo.showEnd);
+});
+
+const ViewPort = useViewPortStore();
+
+interface tcm {
+  name: string;
+  detail: string;
+  picurl?: string;
+}
+
+
+
+// 拿DOM测试，如果图片大小不一致再考虑
+/* let picSize = {
+  picWidth: 0,
+  picHeight: 0,
+}
+const testDOM = useTemplateRef("testDOM");
+const listImage = useTemplateRef("listImage");
+onMounted(() => {
+  console.log(testDOM.value);
+  console.log(listImage.value[0]);
+}); */
 </script>
 
 <style scoped>
@@ -132,9 +186,5 @@ let list = reactive<tcm[]>([
   display: flex;
   align-items: center;
   justify-content: center;
-}
-
-.list-image {
-  
 }
 </style>
