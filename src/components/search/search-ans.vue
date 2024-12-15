d22d02aada56eb02fbb72b79407fee3849f3db03
363045841 <161981174@qq.com>
2024-12-15 17:01:15 +0800

    ✨ feat(search): 历史记录相关功能实现

--- src/components/search/search-ans.vue
+++ src/components/search/search-ans.vue
@@ -1,6 +1,6 @@
 <template>
   <v-list
-    v-if="loadingAttr == false"
+    v-if="loadingAttr == false && props.searchText.length > 0"
     lines="two"
     class="search-list"
     rounded
@@ -12,14 +12,14 @@
       :key="index"
       :subtitle="item.subtitle"
       :prepend-avatar="item.avatar"
-      @click.stop="clickSearchItem"
+      @click.stop="clickSearchItem(item)"
     >
       <template #title>
         <span>
           {{
             item.title.substring(
               0,
-              dataStore.strongTitle.get(item.id)?.[0] ?? 0
+              dataStore.strongTitle.get(item.id)?.[0] ?? 0,
             )
           }}
         </span>
@@ -27,7 +27,7 @@
           ><b>{{
             item.title.substring(
               dataStore.strongTitle.get(item.id)?.[0] ?? 0,
-              dataStore.strongTitle.get(item.id)?.[1] ?? 0
+              dataStore.strongTitle.get(item.id)?.[1] ?? 0,
             )
           }}</b></span
         >
@@ -40,7 +40,7 @@
           {{
             item.subtitle.substring(
               0,
-              dataStore.strongSubTitle.get(item.id)?.[0] ?? 0
+              dataStore.strongSubTitle.get(item.id)?.[0] ?? 0,
             )
           }}
         </span>
@@ -48,19 +48,50 @@
           ><b>{{
             item.subtitle.substring(
               dataStore.strongSubTitle.get(item.id)?.[0] ?? 0,
-              dataStore.strongSubTitle.get(item.id)?.[1] ?? 0
+              dataStore.strongSubTitle.get(item.id)?.[1] ?? 0,
             )
           }}</b></span
         >
         <span>{{
           item.subtitle.substring(
-            dataStore.strongSubTitle.get(item.id)?.[1] ?? 0
+            dataStore.strongSubTitle.get(item.id)?.[1] ?? 0,
           )
         }}</span>
       </template>
     </v-list-item>
   </v-list>
-  <!-- 后面用插槽实现,在search组件上控制加载进度 -->
+  <v-list
+    v-if="props.searchText.length == 0"
+    class="search-list"
+    rounded
+    elevation="1"
+    lines="two"
+  >
+    <div style="margin-top: 16px">
+      <h5 style="margin-left: 24px; margin-right: 4px; display: inline">
+        历史搜索记录
+      </h5>
+      <v-btn
+        density="compact"
+        icon="mdi-pencil"
+        size="small"
+        flat
+        @click="historyListClosable = !historyListClosable"
+      ></v-btn>
+    </div>
+    <v-container>
+      <v-chip
+        v-for="(item, index) in historyListVList"
+        :key="index"
+        style="margin: 4px"
+        @click:close="deleteHistoryListStorage(item)"
+        :closable="historyListClosable"
+      >
+        {{ item }}
+      </v-chip>
+    </v-container>
+  </v-list>
+  <!-- 后面也许用插槽实现,在search组件上控制加载进度 -->
   <loading v-if="loadingAttr"></loading>
 </template>
 
@@ -70,18 +101,60 @@ import { useComponentsSearchItemStore } from "@/stores/searchPage/searchItem";
 import { type TitleInfo } from "@/stores/searchPage/searchItem";
 import loading from "./search-loading.vue";
 
-function clickSearchItem(): void {
-  console.log("点击搜索项");
-}
-
+let historyListClosable = ref<boolean>(false);
 
 const dataStore = useComponentsSearchItemStore();
 const props = defineProps({
   searchText: {
+    // 搜索关键词prop
     type: String,
     default: "",
   },
 });
+
+// 历史记录相关功能
+if (localStorage.getItem("historyList") == null) {
+  localStorage.setItem("historyList", JSON.stringify([]));
+}
+
+function updateHistoryListStorage(newItem: string) {
+  try {
+    let historyList: string[] | null = JSON.parse(
+      localStorage.getItem("historyList") || "[]",
+    );
+    if (historyList != null) {
+      if (!historyList.includes(newItem)) {
+        historyList.push(newItem);
+        localStorage.setItem("historyList", JSON.stringify(historyList));
+      }
+    }
+  } catch (error) {
+    console.error("Error updating history list:", error);
+  }
+}
+
+function deleteHistoryListStorage(newItem: string) {
+  try {
+    let historyList: string[] | null = JSON.parse(
+      localStorage.getItem("historyList") || "[]",
+    );
+    if (historyList != null) {
+      let it = historyList.indexOf(newItem);
+      if(it != -1) historyList.splice(it, 1);
+      console.log("historyList", historyList);
+      localStorage.setItem("historyList", JSON.stringify(historyList));
+    }
+  } catch (error) {
+    console.error("Error updating history list:", error);
+  }
+}
+    
+
+
+function clickSearchItem(item: TitleInfo) {
+  console.log("clickSearchItem", item.title);
+  updateHistoryListStorage(props.searchText);
+}
 
 // DEMO 模拟搜索加载时间
 let loadingAttr = ref(true);
@@ -89,13 +162,9 @@ setTimeout(() => {
   loadingAttr.value = false;
 }, 500);
 
+// 搜索关键词并渲染搜索结果
+import { debounce } from "lodash"; // 引入防抖函数
 const filteredItems = ref<Array<TitleInfo>>([]);
-
-
-// 监听 searchText 的变化
-
-import { debounce } from 'lodash';
-
 let isFirstSearch: boolean = true;
 // 将函数逻辑封装为一个防抖处理的函数
 const debouncedWatchHandler = debounce(
@@ -117,42 +186,51 @@ const debouncedWatchHandler = debounce(
     // 根据输入内容过滤出匹配项
     if (Array.isArray(templist) && templist.length > 0) {
       filteredItems.value = templist.filter((item) => {
-        return (
-          item.title.includes(val) || item.subtitle.includes(val)
-        );
+        return item.title.includes(val) || item.subtitle.includes(val);
       });
     } else {
       filteredItems.value = dataStore.emptytitle;
     }
 
     if (filteredItems.value.length === 0) {
+      // 没有匹配的搜索结果
       filteredItems.value = dataStore.emptytitle;
     }
     /* console.log("filteredItems", filteredItems.value); */
     dataStore.computeStrongRange(val);
   },
-  500 // 防抖延时
+  500, // 防抖延时
 );
 
-//watch到变化了但是
+// 监听 searchText 的变化
+let historyListVList: string[] | null = JSON.parse(
+  localStorage.getItem("historyList") || "[]",
+);
 watch(
   () => props.searchText,
   async (val) => {
     console.log("watching", val);
-    if(isFirstSearch || val === "") {
+    if (isFirstSearch || val === "") {
       isFirstSearch = false;
-      //展示初始值(like 历史记录)
+      // 渲染历史记录
       filteredItems.value = dataStore.historyAlertTitle;
-    }else{
+      historyListVList = JSON.parse(
+        localStorage.getItem("historyList") || "[]",
+      );
+    } else {
       await debouncedWatchHandler(val); // 调用防抖函数
     }
   },
-  { immediate: true } // 初始立即执行一次，确保初始值能正确显示
+  { immediate: true }, // 初始立即执行一次，确保初始值能正确显示
 );
 
-
+// 异步加载json的静态test数据
 async function getAnsJSON(searchWord: string = ""): Promise<TitleInfo[]> {
-  let backupUrl: string = "http://" + import.meta.env.VITE_IP + ":" + import.meta.env.VITE_BACKEND_PORT;
+  let backupUrl: string =
+    "http://" +
+    import.meta.env.VITE_IP +
+    ":" +
+    import.meta.env.VITE_BACKEND_PORT;
   console.log("backupUrl", backupUrl);
   try {
     const response = await fetch(backupUrl + "/json");
@@ -164,7 +242,6 @@ async function getAnsJSON(searchWord: string = ""): Promise<TitleInfo[]> {
     return dataStore.emptytitle; // 发生错误时返回空标题数组
   }
 }
-
 </script>
 
 <style scoped>
