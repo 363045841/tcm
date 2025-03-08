de2812fc33b64e0148128143da8291621a9f854a
363045841 <161981174@qq.com>
2025-03-08 16:46:02 +0800

    ✨ feat(mine): 支持了配伍气性和六味统计雷达图

---
+++ src/components/mine/radarChart.vue
@@ -0,0 +1,80 @@
+<template>
+  <div ref="chart" style="width: 600px; height: 400px"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import * as echarts from "echarts";
+
+interface Item {
+  name: string;
+  count: number;
+}
+
+let props = defineProps({
+  item: Array<Item>,
+  title: String,
+});
+
+let dataOptions = props.item ? props.item.map((item) => item.count) : [];
+let maxData = Math.max(...dataOptions);
+
+let indicatorOptions = props.item
+  ? props.item.map((item) => {
+      return {
+        name: item.name,
+        max: maxData,
+      };
+    })
+  : [];
+
+const chart = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+  console.log(dataOptions);
+  if (chart.value) {
+    const myChart = echarts.init(chart.value);
+
+    // 配置项
+    const option = {
+      title: {
+        text: props.title,
+        left: "left",
+        textStyle: {
+          fontSize: 15
+        },
+      },
+      tooltip: {},
+      radar: {
+        // 定义雷达图的指示器（维度）
+        indicator: indicatorOptions, // 使用传入的指示器配置
+        shape: "circle", // 雷达图形状：'polygon' 或 'circle'
+        splitNumber: indicatorOptions.length, // 分割段数
+        axisName: {
+          color: "#333", // 轴名称的颜色
+        },
+      },
+      series: [
+        {
+          type: "radar",
+          data: [
+            {
+              value: dataOptions,
+              name: props.title,
+
+            },
+          ],
+          areaStyle: {
+            opacity: 0.3, // 填充区域的透明度
+          },
+        },
+      ],
+    };
+
+    // 设置配置项并渲染图表
+    myChart.setOption(option);
+  }
+});
+</script>
+
+<style scoped></style>
--- src/pages/datamine.vue
+++ src/pages/datamine.vue
@@ -1,21 +1,49 @@
 <template>
   <v-container>
-    <v-text-field
-      v-model="herbInput"
-      clearable
-      label="请输入药材名称"
-      variant="outlined"
-      style="width: 40%; margin-top: 40px; margin-left: 40px"
-    ></v-text-field>
-    <v-btn @click="searchHerbs(herbInput)" class="ml-4">搜索</v-btn>
-
-    <v-data-table
-      v-if="herbsCount.length > 0"
-      :items="herbsCount"
-      :headers="headers"
-      class="mt-4"
-    ></v-data-table>
-    <p v-else class="mt-4 text-center">暂无数据</p>
+    <v-row justify="center" style="margin-bottom: 0px">
+      <v-col cols="8" style="margin-top: 20px">
+        <v-text-field
+          v-model="herbInput"
+          clearable
+          rounded
+          label="请输入药材名称"
+          variant="outlined"
+          :hide-details="true"
+        >
+          <template #append-inner>
+            <v-tooltip text="搜索" location="bottom">
+              <template #activator="{ props }">
+                <img
+                  v-bind="props"
+                  class="icon"
+                  src="@/assets/icon/search.svg"
+                  alt="search"
+                  style="height: 24px; margin-right: 8px"
+                  @click="searchHerbs(herbInput)"
+                />
+              </template>
+            </v-tooltip>
+          </template>
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-data-table
+        v-if="herbsCount.length > 0"
+        :items="herbsCount"
+        :headers="headers"
+        class="mt-4"
+      >
+      </v-data-table>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="6" class="text-center" v-if="natureItem.length > 0"
+        ><radar-chart :item="natureItem" title="配伍药材四气统计"></radar-chart>
+      </v-col>
+      <v-col cols="6" v-if="tasteItem.length > 0">
+        <radar-chart :item="tasteItem" title="配伍药材六味统计"></radar-chart>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -31,21 +59,44 @@ const herbsCount = ref<HerbsCount[]>([]);
 const herbInput = ref<string>("");
 
 const headers = [
-  { title: "药材名称", key: "name" },
-  { title: "数量", key: "count" },
+  { title: "药方名称", key: "recipe_name" },
+  { title: "药材成分", key: "prescription_ingredients" },
 ];
 
-interface Item {
+interface CountItem {
   name: string;
   count: number;
 }
+
+interface natureCount {
+  name: string;
+  count: number;
+}
+interface tasteCount {
+  name: string;
+  count: number;
+}
+interface functionCount {
+  efficacy: string;
+  // count: number;
+}
+
+interface searchRes {
+  count: CountItem[];
+  natureCount: natureCount[];
+  tasteCount: tasteCount[];
+  functionCount: functionCount[];
+}
+
+// radarProps
+let natureItem = ref<natureCount[]>([]);
+let tasteItem = ref<tasteCount[]>([]);
 
 async function searchHerbs(query: string) {
   if (!query) return;
 
   try {
-    let items: Item[] = [];
-    const response = await fetch(
+    const response: searchRes = await fetch(
       `http://${import.meta.env.VITE_IP}:${
         import.meta.env.VITE_BACKEND_PORT
       }/api/v1/etcm/mineCount/${query}`
@@ -54,9 +105,12 @@ async function searchHerbs(query: string) {
       .then((data) => {
         console.log(data);
         herbsCount.value = data;
-
+        return data;
       });
 
+    herbsCount.value = response.count;
+    natureItem.value = response.natureCount;
+    tasteItem.value = response.tasteCount;
     console.log(herbsCount.value);
   } catch (error) {
     console.error("请求失败", error);
