ec56191f66f679cb3a92b8f7a33bc9777616199d
363045841 <161981174@qq.com>
2025-04-13 23:44:55 +0800

    ✨ feat(mine): 支持自定义置信度和提升度参数

--- src/pages/datamine.vue
+++ src/pages/datamine.vue
@@ -1,4 +1,4 @@
-h2<template>
+<template>
   <v-container>
     <!-- 搜索框 -->
     <v-row justify="center" style="margin-bottom: 0px">
@@ -19,8 +19,7 @@ h2<template>
     <v-row justify="start" v-if="herbsCount.length > 0">
       <h2 class="title">包含该药材的方剂</h2>
       <v-data-table :items="herbsCount" :headers="includeHeaders" class="mt-4" items-per-page-text="每页显示条数："
-        :pageText="'{0} - {1} 共 {2} 项'">
-      </v-data-table>
+        :pageText="'{0} - {1} 共 {2} 项'"></v-data-table>
     </v-row>
     <v-row v-if="natureItem.length > 0">
       <h2 class="title">统计图表</h2>
@@ -47,7 +46,6 @@ h2<template>
     <v-row justify="center">
       <v-col cols="6" class="text-center" v-if="natureItem.length > 0">
         <radar-chart :item="natureItem" title="配伍药材四气统计"></radar-chart>
-        <!-- 是否一定要v-if重载?后续考虑优化 -->
       </v-col>
       <v-col cols="6" v-if="tasteItem.length > 0">
         <radar-chart :item="tasteItem" title="配伍药材六味统计"></radar-chart>
@@ -67,8 +65,39 @@ h2<template>
           @start="setTempRange" label="选择区间"></v-range-slider>
       </v-col>
     </v-row>
-    <v-row v-if="ruleItem.length > 0">
+    <v-row v-if="ruleItem.length > 0" align="center">
       <h2 class="title">药材关联规则</h2>
+    </v-row>
+    <v-row v-if="ruleItem.length > 0" align="center" justify="center">
+      <v-col cols="3" class="d-flex align-center">
+        <span style="margin-right: 10px;">支持度 (support)</span>
+        <input-number id="inputNumber" :min="0" :max="1" :step="0.1" v-model:value="ruleSupport" />
+      </v-col>
+      <v-col cols="4" class="d-flex align-center">
+        <span style="margin-right: 10px;">置信度 (confident)</span>
+        <input-number id="inputNumber" :min="0" :max="1" :step="0.1" v-model:value="ruleConfidence" />
+        <v-tooltip location="bottom">
+          <template #activator="{ props }">
+            <v-icon v-bind="props" class="ml-2" style="margin-left: 16px !important;" @click="refreshRuleTable">
+              mdi-refresh
+            </v-icon>
+          </template>
+          <span>重新计算</span>
+        </v-tooltip>
+        <v-tooltip location="bottom">
+          <template #activator="{ props }">
+            <v-icon v-bind="props" class="ml-2" style="margin-left: 16px; cursor: pointer;">
+              mdi-information-outline
+            </v-icon>
+          </template>
+          <span>
+            <b>support</b> 参数表示某一配伍组合在所有药方中出现的频率，<br />
+            它决定了只有当某组合达到预设的出现比例时，才会被视为有效规则，从而反映其普遍性。<br />
+            <b>confident</b> 参数衡量在某种药材出现时其他药材同时出现的概率，<br />
+            它影响筛选出的规则的可靠性和强度；设定较高值可以确保提取出更核心、稳定的配伍规则，但也可能降低挖掘潜在有价值组合的数量。
+          </span>
+        </v-tooltip>
+      </v-col>
     </v-row>
     <v-row>
       <v-col cols="12">
@@ -76,12 +105,12 @@ h2<template>
           <v-data-table v-if="ruleItem.length > 0" :items="ruleItem" :headers="ruleHeaders" class="mt-4"
             items-per-page-text="每页显示条数：" :pageText="'{0} - {1} 共 {2} 项'" :key="ruleDataTableKey">
           </v-data-table>
-
         </v-skeleton-loader>
       </v-col>
     </v-row>
     <v-row justify="center">
-      <rule-graph v-if="ruleItem.length > 0" :item="ruleItem" :limit="5" :name="herbInput" style="width: 100%;"></rule-graph>
+      <rule-graph v-if="ruleItem.length > 0" :item="ruleItem" :limit="5" :name="herbInput"
+        style="width: 100%;"></rule-graph>
     </v-row>
   </v-container>
   <v-snackbar v-model="isSearching">
@@ -90,11 +119,12 @@ h2<template>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch, onMounted } from "vue";
+import { InputNumber } from "ant-design-vue";
 
 onMounted(() => {
   document.body.style.overflow = "auto";
-})
+});
 
 interface CountItem {
   name: string;
@@ -109,6 +139,7 @@ export interface ruleRes {
   lift: string;
   count: string;
 }
+
 interface searchRes {
   count: CountItem[];
   natureCount: CountItem[];
@@ -118,16 +149,16 @@ interface searchRes {
 
 const herbsCount = ref<CountItem[]>([]);
 const herbInput = ref<string>("");
-let isSearching = ref<boolean>(false);
-let isLoadingStandardInfo = ref<boolean>(false);
-let isMergeTinyNatureAttribute = ref<boolean>(false);
-let oldNatureCountItem = ref<CountItem[]>([]);
-let isMergeTinyTasteAttribute = ref<boolean>(false);
-let oldTasteCountItem = ref<CountItem[]>([]);
-let range = ref([0, 15]);
+const isSearching = ref<boolean>(false);
+const isLoadingStandardInfo = ref<boolean>(false);
+const isMergeTinyNatureAttribute = ref<boolean>(false);
+const oldNatureCountItem = ref<CountItem[]>([]);
+const isMergeTinyTasteAttribute = ref<boolean>(false);
+const oldTasteCountItem = ref<CountItem[]>([]);
+const range = ref([0, 15]);
 
-// PREF 这俩逆天watch考虑合并下？
-watch(isMergeTinyNatureAttribute, (newValue, oldValue) => {
+// 监控合并次级属性的开关
+watch(isMergeTinyNatureAttribute, (newValue) => {
   if (newValue && natureItem.value.length > 0) {
     let tempNatureItemMap: Map<string, number> = new Map();
     natureItem.value.forEach((item) => {
@@ -156,7 +187,7 @@ watch(isMergeTinyNatureAttribute, (newValue, oldValue) => {
   }
 });
 
-watch(isMergeTinyTasteAttribute, (newValue, oldValue) => {
+watch(isMergeTinyTasteAttribute, (newValue) => {
   if (newValue && tasteItem.value.length > 0) {
     let tempTasteItemMap: Map<string, number> = new Map();
     tasteItem.value.forEach((item) => {
@@ -222,7 +253,7 @@ const ruleHeaders = [
   { title: "匹配数量", key: "count" },
 ];
 
-// radarProps
+// radarChart 相关数据
 let natureItem = ref<CountItem[]>([]);
 let tasteItem = ref<CountItem[]>([]);
 let functionCountItem = ref<CountItem[]>([]);
@@ -230,6 +261,8 @@ let functionCountItemProp = ref<CountItem[]>([]);
 let ruleItem = ref<ruleRes[]>([]);
 let isLoadingRule = ref<boolean>(false);
 let ruleDataTableKey = ref<number>(0);
+let ruleSupport = ref<number>(0.3);
+let ruleConfidence = ref<number>(0.1);
 
 async function searchHerbs(query: string) {
   if (!query) return;
@@ -238,12 +271,10 @@ async function searchHerbs(query: string) {
   isLoadingRule.value = true;
   try {
     const response: searchRes = await fetch(
-      `${import.meta.env.VITE_IP}:${import.meta.env.VITE_BACKEND_PORT
-      }/api/v1/etcm/mineCount/${query}`
+      `${import.meta.env.VITE_IP}:${import.meta.env.VITE_BACKEND_PORT}/api/v1/etcm/mineCount/${query}`
     )
       .then((res) => res.json())
       .then((data) => {
-        // console.log(data);
         herbsCount.value = data;
         return data;
       });
@@ -259,12 +290,10 @@ async function searchHerbs(query: string) {
     isLoadingStandardInfo.value = false;
 
     const ruleRes: ruleRes[] = await fetch(
-      `${import.meta.env.VITE_IP}:${import.meta.env.VITE_BACKEND_PORT
-      }/api/v1/etcm/mineRule/${query}`
+      `${import.meta.env.VITE_IP}:${import.meta.env.VITE_BACKEND_PORT}/api/v1/etcm/mineRule/${query}?confident=${ruleConfidence.value}&support=${ruleSupport.value}`
     ).then((res) => res.json());
     console.log(ruleRes);
     console.log("herbsCount.value", response.count);
-
     ruleItem.value = ruleRes;
     ruleDataTableKey.value += 1;
   } catch (error) {
@@ -272,6 +301,25 @@ async function searchHerbs(query: string) {
   } finally {
     isSearching.value = false;
     isLoadingStandardInfo.value = false;
+    isLoadingRule.value = false;
+  }
+}
+
+async function refreshRuleTable() {
+  // 若当前输入为空则直接返回
+  if (!herbInput.value) return;
+  ruleItem.value.length = 0;
+  isLoadingRule.value = true;
+
+  try {
+    const response: ruleRes[] = await fetch(
+      `${import.meta.env.VITE_IP}:${import.meta.env.VITE_BACKEND_PORT}/api/v1/etcm/mineRule/${herbInput.value}?confident=${ruleConfidence.value}&support=${ruleSupport.value}`
+    ).then((res) => res.json());
+    ruleItem.value = response;
+    ruleDataTableKey.value += 1;
+  } catch (error) {
+    console.error("规则数据请求失败", error);
+  } finally {
     isLoadingRule.value = false;
   }
 }
